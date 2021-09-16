949afe027bc3e21184cfb84570140e2707e69bfc
김형태 <>
2021-09-16 13:15:37 +0900

    플러터 네이버 로그인 예제 1. pubspec.yaml 파일에 flutter_naver_login: ^1.2.2 의존성 주입.
    2. android/app/src/main/res/values/strings.xml 경로에 strings.xml 파일 생성 후 아래 코드 추가 후 저장.
    <?xml version="1.0" encoding="utf-8"?>
    <resources>
        <string name="client_id">[client_id]</string>
        <string name="client_secret">[client_secret]</string>
        <string name="client_name">sns_로그인</string>
    </resources>
    
    3. android/app/src/main/AndroidManifest.xml 경로에 아래 코드 추가
    <application
            android:label="nlogin1"
            android:icon="@mipmap/ic_launcher">
            <meta-data
                android:name="com.naver.sdk.clientId"
                android:value="여기에 클라이언트 아이디 적으세요." />
            <meta-data
                android:name="com.naver.sdk.clientSecret"
                android:value="여기에 클라이언트 시크릿 키 적으세요." />
             <meta-data
                android:name="com.naver.sdk.clientName"
                android:value="클라이언트 이름 적으세요." />
            <activity
                android:name=".MainActivity"
    
    설정은 끝났습니다.

---
+++ nlogin1/lib/page/Login/Login.dart
@@ -0,0 +1,129 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_naver_login/flutter_naver_login.dart';
+import 'dart:async';
+
+class Login extends StatefulWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  bool isLogin = false;
+  String? accesToken;
+  String? expiresAt;
+  String? tokenType;
+  String? name;
+  var user;
+  String? refreshToken;
+
+  Future<void> buttonLoginPressed() async {
+    NaverLoginResult res = await FlutterNaverLogin.logIn();
+    setState(() {
+      name = res.account.nickname;
+      isLogin = true;
+    });
+  }
+
+  Future<void> buttonTokenPressed() async {
+    NaverAccessToken res = await FlutterNaverLogin.currentAccessToken;
+    setState(() {
+      accesToken = res.accessToken;
+      tokenType = res.tokenType;
+    });
+  }
+
+  Future<void> buttonLogoutPressed() async {
+    FlutterNaverLogin.logOut();
+    setState(() {
+      isLogin = false;
+      accesToken = null;
+      tokenType = null;
+      name = null;
+    });
+  }
+
+  Future<void> buttonGetUserPressed() async {
+    NaverAccountResult res = await FlutterNaverLogin.currentAccount();
+    setState(() {
+      name = res.name;
+      user = res;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Flutter Naver Login Sample',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Column(
+              children: <Widget>[
+                Text('isLogin: $isLogin\n'),
+                Text('accesToken: $accesToken\n'),
+                Text('tokenType: $tokenType\n'),
+                Text('user: $user\n'),
+              ],
+            ),
+            FlatButton(
+                key: null,
+                onPressed: buttonLoginPressed,
+                child: const Text(
+                  "LogIn",
+                  style: TextStyle(
+                      fontSize: 12.0,
+                      color: Colors.black,
+                      fontWeight: FontWeight.normal,
+                      fontFamily: "Roboto"),
+                )),
+            FlatButton(
+                key: null,
+                onPressed: buttonLogoutPressed,
+                child: const Text(
+                  "LogOut",
+                  style: TextStyle(
+                      fontSize: 12.0,
+                      color: Colors.black,
+                      fontWeight: FontWeight.normal,
+                      fontFamily: "Roboto"),
+                )),
+            FlatButton(
+                key: null,
+                onPressed: buttonTokenPressed,
+                child: const Text(
+                  "GetToken",
+                  style: TextStyle(
+                      fontSize: 12.0,
+                      color: Colors.black,
+                      fontWeight: FontWeight.normal,
+                      fontFamily: "Roboto"),
+                )),
+            FlatButton(
+                key: null,
+                onPressed: buttonGetUserPressed,
+                child: const Text(
+                  "GetUser",
+                  style: TextStyle(
+                      fontSize: 12.0,
+                      color: Colors.black,
+                      fontWeight: FontWeight.normal,
+                      fontFamily: "Roboto"),
+                ))
+          ]),
+    );
+  }
+}
